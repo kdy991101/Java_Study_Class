753cadca61c6c7e5b860975e267682581599eb3e
kdy991101 <kdy991101@naver.com>
2022-07-12 17:17:57 +0900

    동물 울음 소리 sound class

---
+++ src/com/iu/object2/AnimalSound.java
@@ -0,0 +1,18 @@
+package com.iu.object2;
+
+public class AnimalSound {
+	
+	public void dogSound()
+	{
+		System.out.println("강아지:멍멍");
+	}
+	public void catSound()
+	{
+		System.out.println("고양이:냥냥");
+	}
+	public void tigerSound()
+	{
+		System.out.println("호랑이:어흥");
+	}
+
+}
