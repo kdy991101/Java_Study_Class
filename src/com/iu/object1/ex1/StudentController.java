9f031efa65d59cbb14e37495e8fa051e67525569
kdy991101 <kdy991101@naver.com>
2022-07-12 17:15:07 +0900

    student controller

---
+++ src/com/iu/object1/ex1/StudentController.java
@@ -0,0 +1,20 @@
+package com.iu.object1.ex1;
+
+public class StudentController {
+		public void start() {
+//			1.학생 정보 조회
+//			2.학생 정보 입력
+//			3.학생 정보 검색
+//			4.학생 정보 삭제
+//			5.학생 정보 추가
+//			6.프로그램 종료
+			
+//			 System.out.println("1.학생 정보 입력\n2.학생 정보 검색\n3.학생 정보 삭제\n4.학생 정보 추가\n5.프로그램 종료");
+			System.out.println("1.학생 정보 조회");
+			System.out.println("2.학생 정보 검색");
+			System.out.println("3.학생 정보 삭제");
+			System.out.println("4. 학생 정보 추가");
+			System.out.println("5.프로그램 종료");
+		}	
+		
+}
