22ca91ad11151fe2a721d2fcb2384848810d229d
kdy991101 <kdy991101@naver.com>
2022-07-13 17:20:21 +0900

    최종 출력문

--- src/com/iu/object1/ex1/StudentMain.java
+++ src/com/iu/object1/ex1/StudentMain.java
@@ -8,38 +8,13 @@ public class StudentMain {
 		// TODO Auto-generated method stub
 		System.out.println("시작");
 //		StudentController의 start메서드 호출
-		
-		Scanner sc = new Scanner(System.in);
-		boolean b = true;
-		while (true) {
-		StudentController st = new StudentController();
-		st.start();
-		
-		System.out.println("번호를 입력하세요.");
-		int ch = sc.nextInt();
-		if(ch == 1)
-		{
-			System.out.println("1");
-		}else if (ch == 2)
-		{
-			System.out.println("2");
-		}else if (ch == 3)
-		{
-			System.out.println("3");
-		}else if(ch == 4)
-		{
-			System.out.println("4");
-		}else 
-		{
-			System.out.println("프로그램을 종료합니다");
-			break;
-		}
+		StudentController sc = new StudentController();
+		sc.start();
 		
 
 		System.out.println("종료");
 		}
 	}
 
-	}
 
 
